1ce67598fdf40e7fe2732e27089c8b15c94e7d66
zy1009 <muhammadramzy65@gmail.com>
2023-05-19 21:57:30 +0700

    manage.php

---
+++ manage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Manage</title>
+
+    <style>
+        h1 {
+            text-align: center;
+        }
+
+        .btnTab {
+        background-color: #007bff;
+        color: #fff;
+        border: none;
+        padding: 10px 20px;
+        font-size: 16px;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+      }
+
+      .btnTab:hover {
+        background-color: #0069d9;
+      }
+    </style>
+</head>
+
+<body>
+    <h1>Manage</h1>
+
+    <a class="btnTab" href="manage.php">Manage</a>
+    <a class="btnTab" href="form.php">Fill</a>
+    <a class="btnTab" href="result.php">Result</a>
+</body>
+
+</html>
