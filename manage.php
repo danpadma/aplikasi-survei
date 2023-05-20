0f268703aa6134ed0a508718ed9d45ef10111719
Wildan Mukhtar <ahmadwildan.am@gmail.com>
2023-05-20 09:37:50 +0700

    menambahkan fitur-fitur dalam mengatur pertanyaan (menambah pertanyaan, menghapus pertanyaan, memindahkan pertanyaan ke atas/bawah, memilih tipe jawaban)

--- manage.php
+++ manage.php
@@ -15,56 +15,42 @@
     <h1>Manage</h1>
 
     <!-- change tab -->
-    <a class="btnTab" href="manage.php">Manage</a>
+    <a class="btnTab" id="current-tab" href="manage.php">Manage</a>
     <a class="btnTab" href="form.php">Fill</a>
     <a class="btnTab" href="result.php">Result</a>
 
-    <!-- table manage (short answer) -->
-    <form>
-    <table>
-      <tr>
-        <td>Pertanyaan ...</td>
-        <td><select id="quest_type">
-          <option value="choice">Pilihan ganda</option>
-          <option value="short">Jawaban singkat</option>
-        </select></td>
-      </tr>
-      <tr>
-        <td><input id="answer" type="text"></td>
-      </tr>
-      <tr>
-        <td><a>geser atas</a></td>
-        <td><a>geser bawah</a></td>
-        <td><a>hapus</a></td>
-      </tr>
-    </table>
-
-    <!-- table manage (pilihan) -->
-    <table>
-      <tr>
-        <td>Pertanyaan ...</td>
-        <td><select id="quest_type">
-          <option value="choice">Pilihan ganda</option>
-          <option value="short">Jawaban singkat</option>
-        </select></td>
-      </tr>
-      <tr>
-        <td>
-          <input type="radio" id="pil1" name="pilihan" value="a">
-          <label for="pil1">pilihan 1</label><br>
-          <input type="radio" id="pil2" name="pilihan" value="b">
-          <label for="pil2">pilihan 2</label><br>
-          <input type="radio" id="pil3" name="pilihan" value="c">
-          <label for="pil3">pilihan 3</label><br>
-        </td>
-      </tr>
-      <tr>
-        <td><a>geser atas</a></td>
-        <td><a>geser bawah</a></td>
-        <td><a>hapus</a></td>
-      </tr>
-    </table>
+    <!-- form survei -->
+    <form action="" method="post">
+      <div class="kontainer-survei">
+        <!-- bagian title dan desc -->
+        <input type="text" name="title" id="heading" placeholder="Title" required> <br>
+        <input type="text" name="desc" id="desc" placeholder="Description (optional)">
+        <hr>
+        <!-- pertanyaan2 -->
+        <div class="questions">
+          <div class="question-box">
+            <div class="question">
+              <input type="text" name="question1" placeholder="Question">
+            </div>
+            <div class="answer">
+              <input type="text" name="answer1" placeholder="Your answer" readonly>
+            </div>
+            <div class="utility">
+              <select class="select">
+                <option class="choice" value="Jawaban Singkat">Jawaban Singkat</option>
+                <option class="choice" value="Pilihan Ganda">Pilihan Ganda</option>
+              </select>
+              <button type="button" class="up-btn">^</button>
+              <button type="button" class="down-btn">v</button>
+              <button type="button" class="hapus-btn">hapus</button>
+              <button type="button" class="tambah-btn">tambah</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <input type="submit" name="submit" value="kirim">
     </form>
+    <script src="script.js"></script>
 </body>
 
 </html>
---
+++ script.js
@@ -0,0 +1,112 @@
+// cegah link current tab untuk mereload
+document.getElementById("current-tab").addEventListener("click", function(e) {
+    e.preventDefault()
+})
+
+// ambil questions
+const questions = document.getElementsByClassName("questions")[0]
+
+questions.addEventListener("click", function(e) {
+    // ambil question box
+    const questionBox = e.target.parentElement.parentElement
+    // ambil next sibling question box
+    const nextSibling = questionBox.nextElementSibling
+    // ambil previous sibling question box
+    const prevSibling = questionBox.previousElementSibling
+    // fitur tambah pertanyaan
+    if (e.target.className == "tambah-btn") {
+        // bikin question box baru
+        const qboxAttr = document.createAttribute("class")
+        qboxAttr.value = "question-box"
+        const pertanyaanBaru = document.createElement("div")
+        pertanyaanBaru.setAttributeNode(qboxAttr)
+        // set inner HTML nya
+        pertanyaanBaru.innerHTML = `
+            <div class="question">
+              <input type="text" name="question1" placeholder="Question">
+            </div>
+            <div class="answer">
+              <input type="text" name="answer1" placeholder="Your answer" readonly>
+            </div>
+            <div class="utility">
+              <select class="select">
+                <option class="choice" value="Jawaban Singkat">Jawaban Singkat</option>
+                <option class="choice" value="Pilihan Ganda">Pilihan Ganda</option>
+              </select>
+              <button type="button" class="up-btn">^</button>
+              <button type="button" class="down-btn">v</button>
+              <button type="button" class="hapus-btn">hapus</button>
+              <button type="button" class="tambah-btn">tambah</button>
+            </div>
+        `;
+        // kalo yg dipencet itu pertanyaan terakhir
+        if (nextSibling == null) {
+            // tambah child questions
+            questions.appendChild(pertanyaanBaru)    
+        }
+        // kalo yg dipencet itu bukan pertanyaan terakhir
+        else {
+            // tambah child question sebelum nextSibling
+            questions.insertBefore(pertanyaanBaru, nextSibling)
+        }
+    }
+    // fitur delete (fitur delete ada di bawah fitur add biar childElementCount ga salah)
+    if (e.target.className == "hapus-btn" && questions.childElementCount > 1) { 
+        // remove question box jika tombol hapus dipencet dan child element questions lebih dari 1 (ngga cuma 1 pertanyaan)
+        questionBox.remove()
+    }
+    // fitur geser atas
+    if (e.target.className == "up-btn" && prevSibling != null) {
+        // pindah question box ke sebelum prevSibling. Ini dapat dilakukan jika bukan pertanyaan pertama
+        questions.insertBefore(questionBox, prevSibling)
+    }
+    // fitur geser bawah
+    if (e.target.className == "down-btn" && nextSibling != null) {
+        // pindah next sibling question box ke sebelum question box. Lakukan ini hanya jika bukan pertanyaan terakhir
+        questions.insertBefore(nextSibling, questionBox)
+    }
+    // fitur ubah tipe jawaban
+    if (e.target.className == "select") {
+        e.target.addEventListener("change", function() {
+            // jalankan kode jika state select berubah
+            // ambil class answer
+            const answer = e.target.parentElement.previousElementSibling
+            // kalo milih jawaban singkat
+            if (e.target.value == "Jawaban Singkat") {
+                answer.innerHTML = `
+                    <input type="text" name="answer1" placeholder="Your answer" readonly>
+                `;
+            }
+            // kalo milih pilihan ganda
+            else if (e.target.value == "Pilihan Ganda") {
+                answer.innerHTML = `
+                    <div class="kontainer-pilihan">
+                        <div>
+                            <input type="radio" placeholder="Your answer" disabled>
+                            <input type="text" placeholder="option">
+                        </div>
+                    </div>
+                    <a href="" class="add-option">add option</a>
+                `;
+            }
+        })
+    }
+    // fitur tambah opsi pada pilihan ganda
+    if (e.target.className == "add-option") {
+        const opsiBaru = document.createElement("div")
+        opsiBaru.innerHTML = `
+            <input type="radio" placeholder="Your answer" disabled>
+            <input type="text" placeholder="option">
+            <a href="" class="del-option">X</a>
+        `;
+        // tambah child kontainer pilihan, opsi
+        e.target.previousElementSibling.appendChild(opsiBaru)
+        // biar link pas dipencet ngga reload page
+        e.preventDefault()
+    }
+    // fitur hapus opsi pada pilihan ganda
+    if (e.target.className == "del-option") {
+        e.target.parentElement.remove()
+        e.preventDefault()
+    }
+})
